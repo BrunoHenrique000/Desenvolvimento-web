0c6bd42435134e9780a1bca0cd4bcf8c221018b9
Bruno <bhsilva2@minha.fag.edu.br>
2026-03-25 20:59:28 -0300

    Entrega atv

---
+++ aulas/aula 5/tiposrefs.js
@@ -0,0 +1,48 @@
+
+// objetos
+const aluno = {
+    nome: "Grazielli",
+    idade: 20,
+    curso: "Eng. Software",
+    matriculaAtiva: true,
+    endereco: {
+        rua: "blablabla"
+    }
+}
+
+console.log(aluno.endereco)
+
+//arrays
+const listaFrutas = ["Uva", "Laranja"]
+
+for (let index = 0; index < listaFrutas.length; index++) {
+    const element = listaFrutas[index];
+    console.log("Fruta " + index, element)
+}
+
+// console.log(listaFrutas);
+
+//funções
+const NOME = function printNome() {
+    console.log("Bruno")
+}
+
+NOME();
+
+function soma(val1, val2) {
+    return val1 + val2;
+}
+
+console.log(soma(2, 5))
+
+function subtração(val1, val2) {
+    return val1 - val2;
+}
+
+console.log
+
+function calculo(val1, val2) {
+    return console.log(val1, val2)
+}
+
+console.log(calculo(subtração(2, 4), (soma)(8, 9)))
